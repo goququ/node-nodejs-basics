201ed3002523935407eaf229671ae9909d394ed8
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-04 16:52:37 +0500

    fs/rename finished

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { isFileExist, getDirNameFromMetaUrl } from "./utils/index.js";
+
 const rename = async () => {
-    // Write your code here 
+  const CURRENT_DIRNAME = getDirNameFromMetaUrl(import.meta.url);
+  const FILEPATH = path.resolve(CURRENT_DIRNAME, "files", "wrongFilename.txt");
+  const NEW_FILEPATH = path.resolve(
+    CURRENT_DIRNAME,
+    "files",
+    "properFilename.md"
+  );
+
+  const isSourceExist = await isFileExist(FILEPATH);
+  const isDistFileExist = await isFileExist(NEW_FILEPATH);
+
+  if (!isSourceExist || isDistFileExist) {
+    throw new Error(`FS operation failed`);
+  }
+
+  fs.rename(FILEPATH, NEW_FILEPATH);
 };
 
 await rename();

38c400a73b8a1322c1c946936f9c8602363f8d01
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-04 21:49:37 +0500

    cli/env finished

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,19 @@
+import process from "node:process";
+
+const PREFIX = "RSS_";
 const parseEnv = () => {
-    // Write your code here 
+  let rssEnvs = Object.entries(process.env).reduce((acc, [key, value]) => {
+    if (key.startsWith(PREFIX)) {
+      acc += `${key}=${value};`;
+    }
+    return acc;
+  }, "");
+
+  if (rssEnvs.endsWith(";")) {
+    rssEnvs = rssEnvs.slice(0, rssEnvs.length - 1);
+  }
+
+  console.log(rssEnvs);
 };
 
 parseEnv();

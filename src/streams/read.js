284e842b90cd2cf3aa6940858b5c9cff1cdaa061
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-04 23:05:12 +0500

    streams/read finished

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+import process from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "files",
+    "fileToRead.txt"
+  );
+  const rr = await fs.createReadStream(filePath);
+
+  rr.on("data", (data) => {
+    process.stdout.write(data.toString("utf-8"));
+  });
 };
 
 await read();

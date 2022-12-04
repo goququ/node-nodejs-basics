e748b14b906890f0eaa7037e664c28ce99e980f5
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-04 22:48:21 +0500

    hash/calcHash finished

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,22 @@
+import crypto from "node:crypto";
+import fs from "node:fs/promises";
+import path from "node:path";
+import url from "node:url";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const pathToFile = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "files",
+    "fileToCalculateHashFor.txt"
+  );
+  const fileBuffer = await fs.readFile(pathToFile);
+  const hashSum = crypto.createHash("sha256");
+
+  hashSum.update(fileBuffer);
+
+  const hex = hashSum.digest("hex");
+
+  console.log(hex);
 };
 
 await calculateHash();

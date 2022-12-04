83aff79b509b7433aa84a02ac0ff58ae31b38f62
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-04 21:34:46 +0500

    fs/read finished

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,19 @@
+import fs from "node:fs/promises";
+import path from "node:path";
+import { isFileOrDirExist, getDirNameFromMetaUrl } from "./utils/index.js";
+
 const read = async () => {
-    // Write your code here 
+  const currentDir = getDirNameFromMetaUrl(import.meta.url);
+  const filePath = path.resolve(currentDir, "files", "fileToRead.txt");
+  const isFileExist = await isFileOrDirExist(filePath);
+
+  if (!isFileExist) {
+    throw new Error("FS operation failed");
+  }
+
+  const content = await fs.readFile(filePath, { encoding: "utf-8" });
+
+  console.log(content);
 };
 
 await read();

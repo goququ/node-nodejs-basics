384056ccdd27f467300cd546573ca3de87df239a
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-05 21:41:52 +0500

    streams/transform finished

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,27 @@
+import process from "node:process";
+import fs from "node:fs";
+import path from "node:path";
+import url from "node:url";
+import stream from "node:stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const filePath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "files",
+    "fileToWrite.txt"
+  );
+  const writableStream = fs.createWriteStream(filePath);
+
+  const res = await stream.pipeline(
+    process.stdin,
+    async function* (source, { signal }) {
+      source.setEncoding("utf8");
+      for await (const chunk of source) {
+        yield [...chunk.toString()].reverse().join("");
+      }
+    },
+    writableStream
+  );
 };
 
 await transform();

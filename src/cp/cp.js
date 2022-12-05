627c9e578a904c4c50207f08311764d38b74f09c
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-05 23:38:00 +0500

    cp/cp finished

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,22 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
+import childProcess from "node:child_process";
+import path from "node:path";
+import url from "node:url";
+import process from "node:process";
+
+const spawnChildProcess = async (args = ["123"]) => {
+  const scriptPath = path.resolve(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    "files",
+    "script.js"
+  );
+
+  const child = childProcess.fork(scriptPath, args, {
+    stdio: ["pipe", "pipe", "pipe", "ipc"],
+  });
+
+  child.stdout.on("data", (data) => {
+    process.stdout.write(data.toString());
+  });
 };
 
 spawnChildProcess();

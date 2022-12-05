be679202532d97baf16e760ad16c4764ef3f2270
Ruslan.Nasyrov <goququ@ya.ru>
2022-12-05 22:24:44 +0500

    wt/worker finished

--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,28 @@
+import {
+  isMainThread,
+  Worker,
+  workerData,
+  parentPort,
+} from "node:worker_threads";
+import url from "node:url";
+
 // n should be received from main thread
-const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
+const nthFibonacci = (n) =>
+  n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+  if (isMainThread) {
+    const worker = new Worker(url.fileURLToPath(import.meta.url), {
+      workerData: 40,
+    });
+
+    worker.on("message", (res) => {
+      console.log(`Main thread received: ${res}`);
+      worker.terminate();
+    });
+  } else {
+    parentPort.postMessage(nthFibonacci(workerData));
+  }
 };
 
 sendResult();
